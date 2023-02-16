f7daae92e9d06ed5d36d00d1a61cd3bce2f5579f
Leoesbravo <lescogidobravo@outlook.com>
2023-02-16 12:54:14 -0600

    MVC

---
+++ PLMVC/Controllers/MateriaController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PLMVC.Controllers
+{
+    public class MateriaController : Controller
+    {
+        // GET: Materia
+        //ActionResult, Action Method, Razor, Bootstrap
+        public ActionResult GetAll()
+        {
+            return View();
+        }
+    }
+}
